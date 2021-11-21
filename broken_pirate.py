baff7e9a4327c5708411b900eeb20843ab4b54a2
azrin101 <fathimaazrin192@gmail.com>
2021-11-21 21:30:05 +0400

    made necessary changes to the file

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,9 @@
+# Added a double quote after the statement ended
 greeting = input("Hello, possible pirate! What's the password?")
-if greeting == "Arrr!":
-        print("Go away, pirate.")
+# replaced the round bracket with a square bracket
+if greeting in ["Arrr!"]:
+    print("Go away, pirate.")
+# replaced the elif statement with an else statement
 else:
-        print("Greetings, hater of pirates!")
+    # indented the line of code correctly
+    print("Greetings, hater of pirates!")
